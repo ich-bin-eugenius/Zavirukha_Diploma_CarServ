c15064309bd1a161712f427c5c19461eb206bc35
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2026-05-11 18:00:21 +0200

    Removing unused values

--- CarServ_Config/settings.py
+++ CarServ_Config/settings.py
@@ -34,12 +34,6 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PORT = os.getenv('DB_PORT')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
